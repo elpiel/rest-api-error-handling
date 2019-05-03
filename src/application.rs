9b0e3d55de19fb2bae6f94431c897529211018ac
Lachezar Lechev <lachezar@adex.network>
2019-05-03 12:34:00 +0300

    The ability to convert to ApplicationError from the underlying Errors

--- src/application.rs
+++ src/application.rs
@@ -5,3 +5,15 @@ pub enum ApplicationError {
     Persistence(PersistenceError),
     Http(HttpError),
 }
+
+impl Into<ApplicationError> for PersistenceError {
+    fn into(self) -> ApplicationError {
+        ApplicationError::Persistence(self)
+    }
+}
+
+impl Into<ApplicationError> for HttpError {
+    fn into(self) -> ApplicationError {
+        ApplicationError::Http(self)
+    }
+}
